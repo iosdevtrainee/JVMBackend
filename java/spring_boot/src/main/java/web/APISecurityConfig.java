d0171f75ee943cb8f8dc0f6be9952dca9af0d37f
iosdevrookie <dev@iosdevrokiesMBP.fios-router.home>
2019-08-14 22:47:21 -0400

    Added security config to REST API

---
+++ java/spring_boot/src/main/java/web/APISecurityConfig.java
@@ -0,0 +1,24 @@
+package web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class APISecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/", "/greeting").permitAll()
+                .anyRequest().authenticated();
+
+    }
+}
